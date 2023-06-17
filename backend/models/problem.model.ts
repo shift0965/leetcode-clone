684488008d5acf472538d21047b8ffbf42fe8219
Jackie Chen <jackie@Jackies-MacBook-Air.local>
2023-06-17 11:01:13 +0800

    feat: add tags and solution into getAllProblems and getProblemDetails api
    Each problems has a list of tags related to the algorithm and a youtube solution video code

--- backend/models/problem.model.ts
+++ backend/models/problem.model.ts
@@ -41,34 +41,65 @@ export async function getExampleCasesDataById(problemId: number) {
 }
 
 export async function getAllProblems() {
-  const results = await pool.query("SELECT id, title, difficulty FROM problem");
+  const results = await pool.query(
+    "SELECT p.id, p.title, p.difficulty, p.solution_video, t.id AS tag_id, t.title AS tag FROM problem AS p LEFT JOIN problem_to_tag AS pt ON p.id = pt.problem_id LEFT JOIN tag AS t ON t.id = pt.tag_id"
+  );
+  console.log(results[0]);
   const problems = z.array(ProblemSchema).parse(results[0]);
-  return problems;
+  const problemMap = new Map();
+  problems.forEach((problem) => {
+    const problemObj = problemMap.get(problem.id) || {
+      id: problem.id,
+      title: problem.title,
+      difficulty: problem.difficulty,
+      solutionVideo: problem.solution_video,
+      tags: [],
+    };
+    problemObj.tags.push({ id: problem.tag_id, title: problem.tag });
+    problemMap.set(problem.id, problemObj);
+  });
+
+  return Array.from(problemMap.values());
 }
 
 export async function getProblemDetailsById(problemId: number) {
-  const results = await pool.query(
-    "SELECT p.id, p.title, p.difficulty, p.constraints, p.input_keys, p.boilerplate, pe.input, pe.output, pe.explanation, pe.image FROM problem AS p LEFT JOIN problem_example AS pe ON p.id = pe.problem_id WHERE p.id = ?",
+  const problemResults = await pool.query(
+    "SELECT p.id, p.title, p.description, p.difficulty, p.constraints, p.input_keys, p.boilerplate FROM problem AS p WHERE p.id = (?)",
     [problemId]
   );
-  const problemDetails = z.array(ProblemDetailsSchema).parse(results[0]);
+
+  const exampleCaseResults = await pool.query(
+    "SELECT pe.input, pe.output, pe.explanation, pe.image FROM problem_example AS pe WHERE pe.problem_id = (?)",
+    problemId
+  );
+  const tagResults = await pool.query(
+    "SELECT t.id, t.title FROM problem_to_tag AS pt LEFT JOIN tag AS t on t.id = pt.tag_id WHERE pt.problem_id = (?)",
+    [problemId]
+  );
+
+  const problemDetails = z.array(ProblemDetailsSchema).parse(problemResults[0]);
+  const exampleCases = z
+    .array(ProblemDetailsExampleCasesSchema)
+    .parse(exampleCaseResults[0]);
+  const tags = z.array(TagSchema).parse(tagResults[0]);
+
   if (problemDetails.length === 0) return null;
 
   const problem = {
-    id: problemDetails[0]?.id,
-    title: problemDetails[0]?.title,
-    difficulty: problemDetails[0]?.difficulty,
-    constraints: JSON.parse(problemDetails[0]?.constraints || "{}"),
-    input_keys: JSON.parse(problemDetails[0]?.input_keys || "{}"),
-    boilerplate: problemDetails[0]?.boilerplate,
-    exampleCases: problemDetails.map((item) => {
-      return {
-        input: JSON.parse(item.input),
-        output: JSON.parse(item.output),
-        explanation: item.explanation,
-        image: item.image,
-      };
-    }),
+    id: problemDetails[0].id,
+    title: problemDetails[0].title,
+    difficulty: problemDetails[0].difficulty,
+    description: problemDetails[0].description,
+    constraints: JSON.parse(problemDetails[0].constraints || "{}"),
+    input_keys: JSON.parse(problemDetails[0].input_keys || "{}"),
+    boilerplate: problemDetails[0].boilerplate,
+    exampleCases: exampleCases.map((exampleCase) => ({
+      input: JSON.parse(exampleCase.input),
+      output: JSON.parse(exampleCase.output),
+      explanation: exampleCase.explanation,
+      image: exampleCase.image,
+    })),
+    tags: tags,
   };
   return problem;
 }
@@ -79,21 +110,35 @@ const ProblemSchema = z.object({
   id: z.number(),
   title: z.string(),
   difficulty: DifficultySchema,
+  solution_video: z.string().nullable(),
+  tag: z.string().nullable(),
+  tag_id: z.number().nullable(),
 });
 
+//get Problem details
 const ProblemDetailsSchema = z.object({
   id: z.number(),
   title: z.string(),
   difficulty: DifficultySchema,
+  description: z.string(),
   constraints: z.string().nullable(),
   input_keys: z.string(),
   boilerplate: z.string(),
+});
+
+const ProblemDetailsExampleCasesSchema = z.object({
   input: z.string(),
   output: z.string(),
   explanation: z.string().optional().nullable(),
   image: z.string().nullable(),
 });
 
+const TagSchema = z.object({
+  id: z.number(),
+  title: z.string(),
+});
+
+//run example cases
 const ExampleCasesDataSchema = z.object({
   function_name: z.string(),
   input: z.string(),
