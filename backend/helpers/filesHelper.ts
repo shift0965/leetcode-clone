a729e02089b67c83602493c0b280bef3251ebe29
Jackie Chen <jackie@Jackies-MacBook-Air.local>
2023-06-27 17:21:02 +0800

    update: add one line infront of funciton code to prevend user typing import in code mirror

--- backend/helpers/filesHelper.ts
+++ backend/helpers/filesHelper.ts
@@ -18,23 +18,33 @@ export async function generateFile(
     fs.mkdirSync(dirCode, { recursive: true });
   }
 
-  const extension = `
-  const consoles = [];
+  const prefix = `const consoles = [];
   console.log = function (message) {
-    ${ifRecordConsole && "consoles.push(message)"}
+    if(${ifRecordConsole}){
+      if(message.typeof === "string"){
+        consoles.push(message)
+      }
+      else{
+        consoles.push(JSON.stringify(message))
+      }
+    }
     return ;
   };
-
-  const args = JSON.parse(process.argv[2])
+  const args = JSON.parse(process.argv[2])`;
+  const postfix = `
   const output = ${functionName}(...args);
   process.stdout.write(JSON.stringify(${
     ifRecordConsole ? "{consoles, output}" : "{output}"
   }));
   `;
-
   const filename = `${jobId}.${format}`;
   const filePath = path.join(dirCode, filename);
-  fs.writeFileSync(filePath, content + extension);
+  fs.writeFileSync(
+    filePath,
+    prefix.trim().replace(/\n/g, " ") +
+      content +
+      postfix.trim().replace(/\n/g, " ")
+  );
   return filePath;
 }
 
