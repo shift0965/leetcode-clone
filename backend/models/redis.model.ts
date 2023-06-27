496adfb3b4f572a5d0267f757283686b41e366f5
Jackie Chen <jackie@Jackies-MacBook-Air.local>
2023-06-27 17:44:00 +0800

    update: use env variable in backend ioredis

--- backend/models/redis.model.ts
+++ backend/models/redis.model.ts
@@ -1,8 +1,10 @@
 import { Redis } from "ioredis";
+import dotenv from "dotenv";
+dotenv.config();
 
 const client = new Redis({
-  host: "localhost",
-  port: 6379,
+  host: process.env.REDIS_HOST || "localhost",
+  port: Number(process.env.REDIS_PORT) || 6379,
 });
 
 export function publishPlayerJoinContest(
