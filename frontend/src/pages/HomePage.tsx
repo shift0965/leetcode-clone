4827b23e7bd3bc55eabc498c5e4850444f1c11c6
Jackie Chen <jackie@Jackies-MacBook-Air.local>
2023-07-02 22:08:54 +0800

    update: add git action yml file

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -26,7 +26,7 @@ const HomePage = () => {
           <h1 className="text-xl text-left text-gray-200 ml-1">Let's Code!</h1>
           <div className=" flex mt-5">
             <button onClick={handleCreateGame}>
-              <div className="h-32 w-48 rounded-xl text-white relative bg-dark-fill-2 flex items-end justify-center">
+              <div className="h-32 w-48 rounded-xl text-white relative bg-dark-fill-3 transition-all hover:bg-dark-fill-2 hover:scale-105 flex items-end justify-center">
                 <img src="createGame.svg" className="h-24 object-contain" />
                 <div className="absolute top-[20%] left-[50%] translate-x-[-50%] translate-y-[-50%] whitespace-pre text-xl opacity-70">
                   Create Game
@@ -34,7 +34,7 @@ const HomePage = () => {
               </div>
             </button>
             <Link to={"gamePlayer"} className=" ml-8">
-              <div className="h-32 w-48 rounded-xl text-white relative bg-dark-fill-2">
+              <div className="h-32 w-48 rounded-xl text-white relative bg-dark-fill-3 transition-all hover:bg-dark-fill-2 hover:scale-105">
                 <img
                   src="joinGame.svg"
                   className="h-full w-full object-contain"
