28c75787119982a84a65f1a59875382df8073822
Jackie Chen <jackie@Jackies-MacBook-Air.local>
2023-07-24 14:32:59 +0800

    update: add type code into editor with in testing

--- testingE2E/main.py
+++ testingE2E/main.py
@@ -5,24 +5,42 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import time
 
 
-options = Options()
-options.add_experimental_option("detach", True)
+def solveTwoSumWithConsole(driver):
+    resetCode = driver.find_element(By.CSS_SELECTOR, "[aria-label='reset code']")
+    codeMirror = driver.find_element(By.ID, "code editor")
+    codeLines = codeMirror.find_elements(By.CLASS_NAME, "cm-line")
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-driver.get("https://letscode.courater.com")
-# driver.maximize_window()
-wait = WebDriverWait(driver, 10) 
-# problem finished loading
-element = wait.until(EC.visibility_of_element_located((By.TAG_NAME, "table")))
+    # start action
+    action = ActionChains(driver)
+    action.click(resetCode)
+    action.pause(1)
+    action.click(codeLines[4])
+    action.send_keys(
+        "\nvar obj = {}; \nconsole.log(nums); \nfor(var i=0; i<nums.length; i++){ \nif(obj[nums[i]] !== undefined){ \nreturn [obj[nums[i]], i];"
+    )
+    action.pause(1)
+    action.send_keys(Keys.DOWN, Keys.ENTER)
+    action.pause(1)
+    action.send_keys("else \nobj[target - nums[i]] = i;")
+    action.perform()
 
-links = driver.find_elements(By.TAG_NAME, 'a')
-for link in links:
-    #print(a.get_attribute("innerHTML"))
-    if link.text == "Valid Parentheses":
-        link.click()
 
-        codeMirror = driver.find_element(By.CLASS_NAME, "codeMirror")
-        break
-    print(link.text)
+def testTwoSum():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://letscode.courater.com")
+    twoSumLink = driver.find_element(By.CSS_SELECTOR, "[aria-label='Two sum']")
+    twoSumLink.click()
+    time.sleep(1)
+    solveTwoSumWithConsole(driver)
+
+    runCode = driver.find_element(By.CSS_SELECTOR, "[aria-label='run]")
+
+    driver.quit()
+
+
+testTwoSum()
